277fcc88ff0a0bf3e9462353718d54b34b9911da
m00n620 <gex6611@gmail.com>
2024-10-23 08:41:00 -0400

    fix buildOpenTradeTx

--- tests/buildTx.ts
+++ tests/buildTx.ts
@@ -9,6 +9,25 @@ async function runTest() {
   try {
     {
       const args = {
+        user: "0x73b3A111C5BCCf9086c97B96e0AbAad69Dc4f523",
+        pairIndex: 0, // BTC/USD
+        collateralAmount: parseUnits("100", 6), // 100 USDC
+        openPrice: 66108.8,
+        long: true, // Long
+        leverage: 2, // 2x
+        tp: 363598.4,
+        sl: 0,
+        collateralIndex: 3, // USDC
+        tradeType: 0, // Market
+        maxSlippage: 1.02,
+      };
+      console.log(`\nOpen Trade`);
+      const tx = await tradingSdk.build.openTrade(args);
+      console.log(tx);
+    }
+
+    {
+      const args = {
         type: ModifyPositionTxType.INCREASE_POSITION_SIZE as UpdatePositionSizeTxType,
         index: 1,
         collateralDelta: parseUnits("1000", 6), // 1000 USDC
